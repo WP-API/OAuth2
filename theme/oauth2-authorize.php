0128cf01a97656d8e6da1d893ed2f68779ba20ac
Ryan McCue <me@ryanmccue.info>
2017-06-19 20:09:35 +1000

    Use actual client data

--- theme/oauth2-authorize.php
+++ theme/oauth2-authorize.php
@@ -76,7 +76,7 @@ $url = $_SERVER['REQUEST_URI'];
 	 * Fires inside the lostpassword <form> tags.
 	 */
 	do_action( 'oauth2_authorize_form', $client );
-	wp_nonce_field( 'json_oauth2_authorize' );
+	wp_nonce_field( sprintf( 'oauth2_authorize:%s', $client->get_key() ) );
 	?>
 
 	<p class="submit">
