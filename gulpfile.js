e597449705b6d336e3ccf693251246b4c797ee6b
Almir Bijedic <almir.bij@gmail.com>
2017-09-05 22:36:22 +0200

    Fixed gulpfile.js phpcs

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ const gulp = require( 'gulp' ),
 	shell = require( 'gulp-shell' ),
 	guppy = require( 'git-guppy' )( gulp );
 
-gulp.task( 'phpcs', shell.task( ['npm run-script phpcs'] ) );
-gulp.task( 'pre-commit', ['phpcs'] );
+gulp.task( 'phpcs', shell.task( [ 'npm run-script phpcs' ] ) );
+gulp.task( 'pre-commit', [ 'phpcs' ] );
